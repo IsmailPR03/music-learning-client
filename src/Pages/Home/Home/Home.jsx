0c50a95f8593cf57df8d4580bca926eee413c71e
rockys1 <rocky.sj26@gmail.com>
2023-10-18 18:26:47 +0600

    ui updated and the project in completed

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,6 +4,8 @@ import PopularClasses from "../PopularClasses/PopularClasses";
 import PopularInstructors from "../PopularInstructors/PopularInstructors";
 import Reviews from "../Reviews/Reviews";
 import useScrollTop from "../../../hooks/useScrollTop";
+import AboutUs from "../AboutUs/AboutUs";
+import './Home.css'
 
 const Home = () => {
     const { pathName } = useLocation();
@@ -13,9 +15,19 @@ const Home = () => {
             <Banner></Banner>
             <div className="w-9/12 mx-auto">
                 <PopularClasses></PopularClasses>
-                <PopularInstructors></PopularInstructors>
             </div>
-            <Reviews></Reviews>
+            <div className="instructor-bg h-full md:h-[1020px]">
+                <div className="w-9/12 mx-auto">
+                    <PopularInstructors></PopularInstructors>
+                </div>
+            </div>
+            <div className="mt-12">
+                <AboutUs></AboutUs>
+            </div>
+
+            <div className="w-11/12 md:w-full mx-auto">
+                <Reviews></Reviews>
+            </div>
         </div>
     );
 };
